a68e1d23ab9bfd699efe50fcb385aebcb5208e22
R21N21 <xnigvmvkvd@gmail.com>
2022-09-20 15:21:43 +0900

    Userのモデル単体テストの実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    lastname              {Gimei.last.kanji}
+    firstname             {Gimei.first.kanji}
+    lastname_kana         {Gimei.last.katakana}
+    firstname_kana        {Gimei.first.katakana}
+    birthday              {Faker::Date.birthday(min_age: 18, max_age: 65)}
+  end
+end
